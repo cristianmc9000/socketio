a4a059d3108574d13a879cfd1b4e945b72e0c270
cristianmc9000 <cristian.preof1@gmail.com>
2022-10-05 10:39:38 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const io = require('socket.io')(3000, {
+	cors: {
+		origin: "http://localhost",
+		methods: ['GET', 'POST'],
+		allowedHeaders: ["my-custom-header"],
+		credentials: true
+	}
+});
+io.on('connection', (socket) => {
+	console.log('Usuario conectado');
+	socket.emit('message', 'Hola mundo');
+	socket.on('disconnect', () => {
+		console.log('Usuario desconectado');
+	})
+	socket.on('chatmsg', msg => {
+		io.emit('message', msg);
+	})
+})
